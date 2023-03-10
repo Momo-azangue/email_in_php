2da27304a201fcca0af89266f73cdf640e6f43fa
Momo-azangue <azanguewill@gmail.com>
2023-03-10 12:09:29 +0100

    php  code

---
+++ src/mail.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Send Mail From Localhost | codingNepal</title>
+    <link rel="stylesheet" href="style.css">
+
+    <!-- bootsrap cdn link  -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+<body>
+    <div class="conatiner">
+        <div class="row">
+            <div class="col-md-4 offset-md-4">
+                <h2 class="text-center">Send Message</h2>
+                <p class="text-center">Send mail ton anyone for localhost.</p>
+                <form action="#" method="POST" autocomplete="off">
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="recipients">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
