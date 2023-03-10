8b22015416bb6e6f51c67f8a05c028ddd98cf268
Momo-azangue <azanguewill@gmail.com>
2023-03-10 12:52:12 +0100

    php  code

--- src/mail.php
+++ src/mail.php
@@ -10,15 +10,25 @@
 
 </head>
 <body>
-    <div class="conatiner">
+    <div class="container">
         <div class="row">
-            <div class="col-md-4 offset-md-4">
+            <div class="col-md-4 offset-md-4 mail-form">
                 <h2 class="text-center">Send Message</h2>
                 <p class="text-center">Send mail ton anyone for localhost.</p>
                 <form action="#" method="POST" autocomplete="off">
                     <div class="form-group">
-                        <input type="email" class="form-control" placeholder="recipients">
+                        <input type="email" class="form-control" placeholder="Recipients">
                     </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Subject">
+                    </div>
+                    <div class="form-group">
+                        <textarea name="" cols="30" rows="10" class="form-control textarea" placeholder="Compose message..."></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="form-control button btn-primary" value="Send">
+                    </div>
+
                 </form>
             </div>
         </div>
